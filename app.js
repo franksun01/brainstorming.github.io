32a4fb753142f4229c9c407b5be9d8c8b900cd37
franksun01 <stuwrkr-fengshi2@ad.uillinois.edu>
2023-10-23 20:04:17 -0500

    added session

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ const firebaseConfig = {
     appId: "1:543716867271:web:022e3f6c562f992b6bd2e2"
 };
 
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
 
-let timerDuration = 300; // 5 minutes in seconds
+let timerDuration = 300;
 let timerInterval;
+let currentSessionID = null;
 
 function startTimer() {
     if(timerInterval) return;
@@ -34,44 +34,56 @@ function startTimer() {
 }
 
 function submitIdea() {
+    if (!currentSessionID) {
+        alert('Please join a session before submitting ideas!');
+        return;
+    }
+
     const idea = document.getElementById('idea').value;
     if (idea.trim() === '') {
         alert('Please enter a valid idea!');
         return;
     }
-    
-    // Push the idea to Firebase
-    database.ref('ideas/').push({
+
+    database.ref('sessions/' + currentSessionID + '/ideas/').push({
         content: idea
     });
 
-    // Clear the idea textarea
     document.getElementById('idea').value = '';
 }
 
 function addIdeaToDisplay(content) {
     const ideasContainer = document.getElementById('ideas-container');
-    const ideaElement = document.createElement('div');
+    ideaElement = document.createElement('div');
     ideaElement.textContent = content;
     ideasContainer.appendChild(ideaElement);
 }
 
-function clearIdeasFromDatabase() {
-    database.ref('ideas/').remove();
+function joinSession() {
+    const sessionID = document.getElementById('session-input').value.trim();
+    if (!sessionID) {
+        alert('Please enter a valid session ID!');
+        return;
+    }
+
+    // Clear any existing displayed ideas.
+    document.getElementById('ideas-container').innerHTML = '';
+
+    currentSessionID = sessionID;
+    document.getElementById('current-session-display').textContent = "Current Session: " + currentSessionID;
+
+    // Attach listener to the specific session.
+    database.ref('sessions/' + currentSessionID + '/ideas/').on('child_added', function(data) {
+        addIdeaToDisplay(data.val().content);
+    }, function(error) {
+        console.error("Error retrieving ideas: ", error);
+    });
 }
 
-database.ref('ideas/').on('child_added', function(data) {
-    addIdeaToDisplay(data.val().content);
-}, function(error) {
-    console.error("Error retrieving ideas: ", error);
-});
-
 document.getElementById('submit-button').addEventListener('click', submitIdea);
-
+document.getElementById('join-session-button').addEventListener('click', joinSession);
 document.getElementById('refresh-button').addEventListener('click', function() {
-    clearIdeasFromDatabase(); // This clears ideas from Firebase
-    location.reload(); // This will refresh the page
+    location.reload();
 });
 
-// Start the timer immediately
 startTimer();
